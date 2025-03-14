eb1883082399b1193422cd1d2c5170d2afcbcf2e
Islombek <104667165+AIDSnyper@users.noreply.github.com>
2025-03-14 22:56:14 +0500

    Update update_bio.py

--- update_bio.py
+++ update_bio.py
@@ -7,9 +7,15 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 session_string = os.getenv("SESSION_STRING")
 
+# Set your preferred timezone
+your_timezone = "Asia/Tashkent"  # Change this to your time zone
+
+# Get the correct time
+tz = pytz.timezone(your_timezone)
+now = datetime.now(tz).strftime("%H:%M")
+
+# Update Telegram bio
 with Client("my_account", api_id=api_id, api_hash=api_hash, session_string=session_string) as app:
-    now = datetime.now().strftime("%H:%M")
     bio_text = f"Is not it {now}?"
-    
     app.update_profile(bio=bio_text)
     print(f"Updated bio to: {bio_text}")
